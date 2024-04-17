600615f79ab255c0e3f07e37154c32e350156009
Lukas <39262485+lukaskornis@users.noreply.github.com>
2024-04-17 18:27:55 +0300

    Better Player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,21 +14,13 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-
-        var hit = Physics2D.Raycast(transform.position, input, 1f,wallLayer);
-        canDash = hit.collider != null && hit.collider.CompareTag("Wall");
-
         var newInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        if (canDash && newInput != Vector2.zero)
+        if (rb.velocity.magnitude < 0.1f && newInput != Vector2.zero)
         {
             input = newInput;
-
-            // convert input to z angle
-            var angle = Mathf.Atan2(input.y, input.x) * Mathf.Rad2Deg;
-            transform.eulerAngles = new Vector3(0, 0, angle + 90);
+            transform.up = -input;
         }
 
         rb.velocity = input * speed;
-
     }
 }
