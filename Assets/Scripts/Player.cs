4f8853e6ffa2200e0a4ac39c95c048f46c255309
pijussJ <pijus.jarusevicius3@gmail.com>
2024-04-18 17:09:28 +0300

    S

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Player : MonoBehaviour
 {
@@ -9,11 +10,34 @@ public class Player : MonoBehaviour
     public GameObject landParticles;
     bool hasLanded;
     public RuleTile coinTile;
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+=======
+    public Tilemap tilemap;
+>>>>>>> 2f6947af84a0977a80a5849849f3cb4efb1759d7
+>>>>>>> Stashed changes
 
+    Sprite coinSprite;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+<<<<<<< Updated upstream
         coinTile.m_DefaultSprite = null;
+=======
+<<<<<<< HEAD
+        coinTile.m_DefaultSprite = null;
+=======
+        coinSprite = coinTile.m_DefaultSprite;
+        coinTile.m_DefaultSprite = null;
+        tilemap.RefreshAllTiles();
+    }
+
+    void OnDestroy()
+    {
+        coinTile.m_DefaultSprite = coinSprite;
+>>>>>>> 2f6947af84a0977a80a5849849f3cb4efb1759d7
+>>>>>>> Stashed changes
     }
 
     void Update()
@@ -36,14 +60,28 @@ public class Player : MonoBehaviour
             obj.transform.up = transform.up;
         }
 
-
         rb.velocity = input * moveSpeed;
     }
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+>>>>>>> Stashed changes
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Coin"))
         {
             Destroy(collision.gameObject);
+<<<<<<< Updated upstream
+=======
+=======
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Coin"))
+        {
+            Destroy(other.gameObject);
+>>>>>>> 2f6947af84a0977a80a5849849f3cb4efb1759d7
+>>>>>>> Stashed changes
         }
     }
 }
